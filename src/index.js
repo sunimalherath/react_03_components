9d52a5f5427e987080c94b60747453653d66231c
sunimalherath <sunimaldv@gmail.com>
2020-08-29 00:15:48 +1000

    Adding parent component

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail"; // ./ means look in the same folder
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="John"
-        timeAgo="Today at 0400"
-        commentText="John's comment"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 1300"
-        commentText="Jane's comment"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Matrix"
-        timeAgo="Yesterday at 0900"
-        commentText="Matrix's comment"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="John"
+          timeAgo="Today at 0400"
+          commentText="John's comment"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 1300"
+          commentText="Jane's comment"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Matrix"
+          timeAgo="Yesterday at 0900"
+          commentText="Matrix's comment"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
