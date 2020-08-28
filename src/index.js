f8d3d233da098ffa22c2a861d59b76c117d85479
sunimalherath <sunimaldv@gmail.com>
2020-08-28 23:45:11 +1000

    adding avatar image to props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail"; // ./ means look in the same folder
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="John" />
-      <CommentDetail author="Jane" />
-      <CommentDetail author="Matrix" />
+      <CommentDetail
+        author="John"
+        timeAgo="Today at 0400"
+        commentText="John's comment"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 1300"
+        commentText="Jane's comment"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Matrix"
+        timeAgo="Yesterday at 0900"
+        commentText="Matrix's comment"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
